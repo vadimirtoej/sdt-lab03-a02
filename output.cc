36183b2685de632f4779a7f8d3d467e04ae2d469
Olga Parfeniova <oligha1996@mail.ru>
2016-10-07 09:59:43 +0000

    added project file

--- output.cc
+++ output.cc
@@ -4,21 +4,34 @@
 
 using namespace std;
 
+
 ostream&
 operator<<(ostream& output, const Transit& transit) {
     // TODO: печатать переход.
+	//Node* c= Transit.destination;
+	Station* station = transit.destination;
+	station->name;
+	output <<"Perexod na station"<< station;	
     return output;
 }
 
 ostream&
 operator<<(ostream& output, const Station& station) {
     // TODO: печатать станцию.
+	Line* line = station.line;
+	line -> name;
+	
+	Transit* transit = station.transits;
+	transit -> destination;
+	
+	output <<"linia"<< line<<"perexod"<<transit;
     return output;
 }
 
 ostream&
 operator<<(ostream& output, const Line& line) {
     // TODO: печатать линию.
+	
     return output;
 }
 
