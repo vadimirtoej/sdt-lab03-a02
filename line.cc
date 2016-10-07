690ac0759eeb409a2b935efcc3e7cd58751208de
Daniil Novikov <diggy-work@yandex.ru>
2016-10-07 07:29:46 +0000

    Initial commit. Find_line func is implemented a bit.

--- line.cc
+++ line.cc
@@ -2,20 +2,29 @@
 
 using namespace std;
 
-Line*
-find_line(Lines& lines, const string& name) {
+Line* find_line(Lines& lines, const string& name) {
     // TODO: искать линию под названием `name` в списке `lines`.
+	bool enter_flag = true;
+	while (true) 
+	{
+		if (enter_flag)
+		{	
+			Line* cur_line = *lines.first;
+			if (cur_line.name == name)
+			return cur_line;
+		}
+		
+			
+	}
     return nullptr;
 }
 
-Line*
-add_line(Lines& lines, const string& name) {
+Line* add_line(Lines& lines, const string& name) {
     // TODO: добавить линию под названием `name` в конец списка `lines`.
     return nullptr;
 }
 
-Line*
-get_line(Lines& lines, const string& name) {
+Line* get_line(Lines& lines, const string& name) {
     // TODO: найти линию под названием `name` в `lines` или создать и добавить её в конец.
     return nullptr;
 }
