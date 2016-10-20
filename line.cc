ee5984bf2013e7c845093fbaf365512d277b236c
Daniil Novikov <diggy-work@yandex.ru>
2016-10-20 23:25:52 +0300

    I've finished line.cc and written some tests in main.cpp to show that everything works as it should.

--- line.cc
+++ line.cc
@@ -2,14 +2,12 @@
 
 using namespace std;
 
-//Line line; //define value 
-//Line* line_ptr; //pointer
-//Line& lin_ref =  
-
-Line* find_line(Lines& lines, const string& name) {
-    // TODO: искать линию под названием `name` в списке `lines`.
+Line* find_line(Lines& lines, const string& name)
+{
+    // Данная функция ищет линию под названием `name` в списке `lines`.
 	Line* cur_line = lines.first;
-	while(cur_line != nullptr){
+	while(cur_line != nullptr)
+    {
 		if (cur_line -> name == name)
 			return cur_line;
 		cur_line = cur_line -> next;
@@ -17,16 +15,27 @@ Line* find_line(Lines& lines, const string& name) {
 	return nullptr;
 }
 
-Line* add_line(Lines& lines, const string& line_name) {
-    // TODO: добавить линию под названием `name` в конец списка `lines`.
-	Line* cur_line;
-//	Line* cur_line = new cur_line;
-    cur_line -> name = line_name;
-    cur_line -> next = nullptr;
-	lines.last -> next = cur_line;
+Line* add_line(Lines& lines, const string& line_name)
+{
+    // Данная функция добавляет линию под названием `name` в конец списка `lines`.
+	Line* line_to_add = new Line;
+    line_to_add -> name = line_name;
+    line_to_add -> next = nullptr;
+	lines.last -> next = line_to_add;
+	lines.last = line_to_add;
 }
 
-Line* get_line(Lines& lines, const string& name) {
-    // TODO: найти линию под названием `name` в `lines` или создать и добавить её в конец.
-    return nullptr;
+Line* get_line(Lines& lines, const string& name)
+{
+    // Данная функция находит линию под названием `name` в `lines` или создаёт и добавляет её в конец.
+    Line* line_to_get = find_line(lines, name);
+    if (line_to_get == nullptr)
+    {
+        add_line(lines, name);
+        return line_to_get;
+    }
+    else
+    {
+        return line_to_get;
+    }
 }
--- main.cpp
+++ main.cpp
@@ -1,31 +1,46 @@
-#include <stdio.h>
+#include <iostream>
 #include "metro.h"
 #include "line.cc"
 
+using namespace std;
+
 int main(int argc, char **argv)
 {
-//	list_init();
-//	Station* station_1;
-//	Station* station_2;
-//	Station* station_3;
-//	station_1.name = "Борисово";
-//	station_1.next = *station_2;
-//	station_1.line = "Люблинско-Дмитровская"
-//	station_2.name = "Шипиловская";
-//	station_2.next = *station_3;
-//	station_2.line = "Люблинско-Дмитровская"
-//	station_3.name = "Зябликово";
-//	station_3.next = nullptr;
-//	station_3.line = "Люблинско-Дмитровская"
-	Line* line_1;
-	Line* line_2;
-	line_1 -> name = "Люблинско-Дмитровская";
-	line_1 -> next = line_2;
-	line_2 -> name = "Кольцевая";
-	line_2 -> next = nullptr;
-	Lines* lines_test;
-	lines_test -> first = line_1;
-	lines_test -> last = line_2;
-	find_line(*lines_test, "Кольцевая");
+    Line* line_1 = new Line;
+    Line* line_2 = new Line;
+    line_1 -> name = "Lublinsko-Dmitrovskaya";
+    line_1 -> next = line_2;
+    line_2 -> name = "Koltsevaya";
+    line_2 -> next = nullptr;
+    Lines* lines_test = new Lines;
+    lines_test -> first = line_1;
+    lines_test -> last = line_2;
+
+    // Тест 1: find_line.
+    Line* answer = find_line(*lines_test, "Koltsevaya");
+    cout << "---Test #1: find_line function---" << '\n' << answer -> name << '\n';
+
+    // Тест 2: add_line.
+    add_line(*lines_test, "Butovskaya");
+    add_line(*lines_test, "Zamoskvoretskaya");
+    Line* cur_line_t = lines_test -> first;
+    cout << "---Test #2: add_line function---" << '\n';
+    while (cur_line_t != nullptr)
+    {
+        cout << cur_line_t -> name << '\n';
+        cur_line_t = cur_line_t -> next;
+    }
+
+    // Тест 3: get_line.
+    cout << "---Test #3: get_line function---" << '\n';
+    cout << "Finding line in lines list: " << get_line(*lines_test, "Koltsevaya") -> name << '\n';
+    cout << "Adding line in lines list. Func returns nullptr and adds the line: "
+         << '\n' << get_line(*lines_test, "Kalininskaya") << '\n';
+    cur_line_t = lines_test -> first;
+    while (cur_line_t != nullptr)
+    {
+        cout << cur_line_t -> name << '\n';
+        cur_line_t = cur_line_t -> next;
+    }
 	return 0;
 }
