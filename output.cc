b850eb6821d1ed76b9f6c2f7627abf2a46d1f54a
Julia Mlukova <julianamor@rambler.ru>
2016-12-01 20:33:30 +0300

    Task completed

--- output.cc
+++ output.cc
@@ -6,24 +6,38 @@ using namespace std;
 
 ostream&
 operator<<(ostream& output, const Transit& transit) {
-    // TODO: печатать переход.
+    output << "\t\ttransit to station " << transit.destination->name << " of " << transit.destination->line->name << " line" << endl;
     return output;
 }
 
 ostream&
 operator<<(ostream& output, const Station& station) {
-    // TODO: печатать станцию.
+    output << "\tstation " << station.name << endl;
+    Transit *current = station.transits.first;
+    while(current != nullptr){
+        output << *current << endl;
+        current = current->next;
+    }
     return output;
 }
 
 ostream&
 operator<<(ostream& output, const Line& line) {
-    // TODO: печатать линию.
+    output << "line " << line.name << endl;
+    Station *current = line.stations.first;
+    while(current != nullptr){
+        output << *current << endl;
+        current = current->next;
+    }
     return output;
 }
 
 ostream&
 operator<<(ostream& output, const Scheme& scheme) {
-    // TODO: печатать схему.
+    Line *current = scheme.first;
+    while(current != nullptr){
+        output << *current << endl;
+        current = current->next;
+    }
     return output;
 }
--- transit.cc
+++ transit.cc
@@ -4,5 +4,20 @@ using namespace std;
 
 void
 add_transit(Station* station, Station* destination) {
-    // TODO: добавить переход со станции `station` на станцию `destination`.
+
+    Transits *tmp_transits = &station->transits;
+
+    Transit *new_transit = new Transit;
+    new_transit->destination = destination;
+    new_transit->next = nullptr;
+
+    if (tmp_transits->first == nullptr){
+        tmp_transits->first = new_transit;
+        tmp_transits->last = new_transit;
+        return;
+    }
+
+    tmp_transits->last->next = new_transit;
+    tmp_transits->last = new_transit;
+    return;
 }
