31a5f9aba8ed4808b13181fc1e124e403a8f5264
ParfenovaOI <oligha1996@mail.ru>
2016-12-02 08:08:49 +0000

    Add files via upload
    added

--- output.cc
+++ output.cc
@@ -4,39 +4,45 @@
 
 using namespace std;
 
-
 ostream&
 operator<<(ostream& output, const Transit& transit) {
     // TODO: печатать переход.
-	//Node* c= Transit.destination;
-	Station* station = transit.destination;
-	station->name;
-	output <<"Perexod na station"<< station;	
+     output << "\t\ttransit to station " << transit.destination->name << " of " << transit.destination->line->name << " line" << endl;
+
     return output;
 }
 
 ostream&
 operator<<(ostream& output, const Station& station) {
     // TODO: печатать станцию.
-	Line* line = station.line;
-	line -> name;
-	
-	Transit* transit = station.transits;
-	transit -> destination;
-	
-	output <<"linia"<< line<<"perexod"<<transit;
+      output << "\tstation " << station.name << endl;
+    Transit *current = station.transits.first;
+    while(current != nullptr){
+        output << *current << endl;
+        current = current->next;
+    }
     return output;
 }
 
 ostream&
 operator<<(ostream& output, const Line& line) {
     // TODO: печатать линию.
-	
+      output << "line " << line.name << endl;
+    Station *current = line.stations.first;
+    while(current != nullptr){
+        output << *current << endl;
+        current = current->next;
+    }
     return output;
 }
 
 ostream&
 operator<<(ostream& output, const Scheme& scheme) {
     // TODO: печатать схему.
+      Line *current = scheme.first;
+    while(current != nullptr){
+        output << *current << endl;
+        current = current->next;
+    }
     return output;
 }
